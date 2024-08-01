9e2e7656868958380169e71db0fe2410ad2aced0
TylerJac <sans.gaming235@gmail.com>
2024-08-01 13:44:44 -0400

    added the music player

--- GameRunner.java
+++ GameRunner.java
@@ -4,10 +4,10 @@ public class GameRunner {
 
 
     public static void main(String[] args) {
-        // play some music
-//        String filepath = "CasinoJazz.wav";
-//        PlayMusic music = new PlayMusic();
-//        music.playMusic(filepath);
+//         play some music
+        String filepath = "CasinoJazz.wav";
+        PlayMusic music = new PlayMusic();
+        music.playMusic(filepath);
 
 
         Scanner sc = new Scanner(System.in);
